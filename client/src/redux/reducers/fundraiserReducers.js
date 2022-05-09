761d49cc2b84bb94c96ed822262ed984f7f99805
Busayo <damayor12@yahoo.com>
2022-05-09 17:57:17 +0200

    feat: fundraiser redux reducer

--- client/src/redux/reducers/fundraiserReducers.js
+++ client/src/redux/reducers/fundraiserReducers.js
@@ -1,4 +1,8 @@
-import { REGISTER_USER, LOGIN_USER, LOGOUT_USER } from '../constants/userConstants';
+import {
+  FUNDRAISER_CREATED,
+  FUNDRAISER_DELETED,
+  FUNDRAISER_EDITED,
+} from '../constants/fundraiserConstants';
 // {
 //     type: 'LOGIN_USER',
 //     payload: payload,
@@ -11,17 +15,25 @@ import { REGISTER_USER, LOGIN_USER, LOGOUT_USER } from '../constants/userConstan
 //   };
 // };
 
-const userReducer = (state = {}, action) => {
+const fundraiserReducer = (state = { fundraisers: [] }, action) => {
   switch (action.type) {
-    case REGISTER_USER:
-      return { ...state, register: action.payload };
-    case LOGIN_USER:
-      return { ...state, loginSuccess: action.payload };
-    case LOGOUT_USER:
-      return {};
+    case FUNDRAISER_CREATED:
+      return { ...state, fundraisers: [state.fundraisers, action.payload] };
+    case FUNDRAISER_EDITED:
+      return {
+        ...state,
+        fundraisers: state.fundraisers.map((fund) =>
+          fund._id === action.payload._id ? action.payload : fund,
+        ),
+      };
+    case FUNDRAISER_DELETED:
+      return {
+        ...state,
+        fundraisers: state.fundraisers.filter((fund) => fund._id !== action.payload._id),
+      };
     default:
       return state;
   }
 };
 
-export default userReducer;
+export default fundraiserReducer;
--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import fundraiserReducer from './reducers/fundraiserReducers';
 import userReducer from './reducers/userReducer';
 
 const persistConfig = {
@@ -10,7 +11,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-
+  fundraiser: fundraiserReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
