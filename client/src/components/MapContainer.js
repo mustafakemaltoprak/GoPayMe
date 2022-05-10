2d6c565ae11a072ee8dcd49ef644f76fc01a040c
Busayo <damayor12@yahoo.com>
2022-05-10 23:36:44 +0200

    feat: leaflet maps

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -1,50 +1,51 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Tooltip, useMap, Popup } from 'react-leaflet';
 // import 'leaflet/dist/leaflet.css';
 
+const MapContainerDiv = ({ data, markerRef, mapRef }) => {
+  const defaultPosition = [48.864716, 2.349];
 
-const MapContainerDiv = () => {
-   const defaultPosition = [48.864716, 2.349];
+  const places = [
+    //  ['LOCATION_1', 11.8166, 122.0942],
+    //  ['LOCATION_2', 11.9804, 121.9189],
+    //  ['LOCATION_3', 10.7202, 122.5621],
+    //  ['LOCATION_4', 11.3889, 122.6277],
+    //  ['LOCATION_5', 10.5929, 122.6325],
 
-   const places = [
-     //  ['LOCATION_1', 11.8166, 122.0942],
-     //  ['LOCATION_2', 11.9804, 121.9189],
-     //  ['LOCATION_3', 10.7202, 122.5621],
-     //  ['LOCATION_4', 11.3889, 122.6277],
-     //  ['LOCATION_5', 10.5929, 122.6325],
+    {
+      title: 'Eiffel Tower',
+      description:
+        'The Eiffel Tower (/ˈaɪfəl/ EYE-fəl; French: tour Eiffel [tuʁ‿ɛfɛl]) is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.',
+      picture:
+        'https://t2.gstatic.com/images?q=tbn:ANd9GcQ8iFga8aDttbI5iBkKgDmqOSV7LdcsVkfDHezlLYWrGDT7OajbZJh4qS1kiNbN1kkUHmz4SWPOS98AC5HubKYyKw',
+      position: [48.858222, 2.2945],
+      seeMoreLink: 'https://en.wikipedia.org/wiki/Eiffel_Tower',
+    },
 
-     {
-       title: 'Eiffel Tower',
-       description:
-         'The Eiffel Tower (/ˈaɪfəl/ EYE-fəl; French: tour Eiffel [tuʁ‿ɛfɛl]) is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.',
-       picture:
-         'https://t2.gstatic.com/images?q=tbn:ANd9GcQ8iFga8aDttbI5iBkKgDmqOSV7LdcsVkfDHezlLYWrGDT7OajbZJh4qS1kiNbN1kkUHmz4SWPOS98AC5HubKYyKw',
-       position: [48.858222, 2.2945],
-       seeMoreLink: 'https://en.wikipedia.org/wiki/Eiffel_Tower',
-     },
+    {
+      title: 'Louvre Museum',
+      description:
+        "The Louvre (English: /ˈluːv(rə)/ LOOV(-rə)[3]), or the Louvre Museum (French: Musée du Louvre [myze dy luvʁ]), is the world's largest art museum and a historic monument in Paris, France. A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement (district or ward). Approximately 38,000 objects from prehistory to the 21st century are exhibited over an area of 72,735 square meters (782,910 square feet). In 2019, the Louvre received 9.6 million visitors, making it the most visited museum in the world.",
+      picture:
+        'https://media-exp1.licdn.com/dms/image/C4D1BAQHRblI0BcMdMg/company-background_10000/0/1552905854220?e=2159024400&v=beta&t=l1ZJjkecqUDP1QjmmdY8TvCJHY5e2YM41UuTWCEKa7c',
+      position: [48.861111, 2.336389],
+      seeMoreLink: 'https://en.wikipedia.org/wiki/Louvre',
+    },
+    {
+      title: 'Notre-Dame de Paris',
+      description:
+        'Notre-Dame de Paris (French: [nɔtʁə dam də paʁi]; meaning "Our Lady of Paris"), referred to simply as Notre-Dame,is a medieval Catholic cathedral on the Île de la Cité in the 4th arrondissement of Paris. The cathedral was consecrated to the Virgin Mary and considered to be one of the finest examples of French Gothic architecture. Its pioneering use of the rib vault and flying buttress, its enormous and colourful rose windows, as well as the naturalism and abundance of its sculptural decoration set it apart from the earlier Romanesque style. Major components that make Notre Dame stand out include its large historic organ and its immense church bells.',
+      picture:
+        'https://t1.gstatic.com/images?q=tbn:ANd9GcTP7Kev8E9D4tRQx70zbwJejhXbrWDSHe4YPnZRm2_iF3yXnB-J8_f6BbJov2HUz1IWVM2tpRLR2QEqkfyEkIsX1Q',
+      position: [48.853, 2.3498],
+      seeMoreLink: 'https://en.wikipedia.org/wiki/Notre-Dame_de_Paris',
+    },
+  ];
+  const showPreview = (place) => {
+    // show place's description
+  };
 
-     {
-       title: 'Louvre Museum',
-       description:
-         "The Louvre (English: /ˈluːv(rə)/ LOOV(-rə)[3]), or the Louvre Museum (French: Musée du Louvre [myze dy luvʁ]), is the world's largest art museum and a historic monument in Paris, France. A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement (district or ward). Approximately 38,000 objects from prehistory to the 21st century are exhibited over an area of 72,735 square meters (782,910 square feet). In 2019, the Louvre received 9.6 million visitors, making it the most visited museum in the world.",
-       picture:
-         'https://media-exp1.licdn.com/dms/image/C4D1BAQHRblI0BcMdMg/company-background_10000/0/1552905854220?e=2159024400&v=beta&t=l1ZJjkecqUDP1QjmmdY8TvCJHY5e2YM41UuTWCEKa7c',
-       position: [48.861111, 2.336389],
-       seeMoreLink: 'https://en.wikipedia.org/wiki/Louvre',
-     },
-     {
-       title: 'Notre-Dame de Paris',
-       description:
-         'Notre-Dame de Paris (French: [nɔtʁə dam də paʁi]; meaning "Our Lady of Paris"), referred to simply as Notre-Dame,is a medieval Catholic cathedral on the Île de la Cité in the 4th arrondissement of Paris. The cathedral was consecrated to the Virgin Mary and considered to be one of the finest examples of French Gothic architecture. Its pioneering use of the rib vault and flying buttress, its enormous and colourful rose windows, as well as the naturalism and abundance of its sculptural decoration set it apart from the earlier Romanesque style. Major components that make Notre Dame stand out include its large historic organ and its immense church bells.',
-       picture:
-         'https://t1.gstatic.com/images?q=tbn:ANd9GcTP7Kev8E9D4tRQx70zbwJejhXbrWDSHe4YPnZRm2_iF3yXnB-J8_f6BbJov2HUz1IWVM2tpRLR2QEqkfyEkIsX1Q',
-       position: [48.853, 2.3498],
-       seeMoreLink: 'https://en.wikipedia.org/wiki/Notre-Dame_de_Paris',
-     },
-   ];
-const showPreview = (place) => {
-  // show place's description
-};
+  console.log('MarkerREF', markerRef, mapRef)
   return (
     <div className="map__container">
       {/* <MapContainer center={defaultPosition} zoom={13} style={{ height: 80, width: 80 }}> */}
@@ -54,15 +55,25 @@ const showPreview = (place) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </MapContainer> */}
-      <MapContainer center={[48.864716, 2.349]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer
+        center={[48.864716, 2.349]}
+        zoom={17}
+        
+        scrollWheelZoom={true}
+        whenCreated={(map) => {
+          mapRef.current = map;
+        }}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {places.map((place) => (
+        {data.map((place) => (
           <Marker
+          
+            ref={markerRef}
             key={place.id}
-            position={place.position} // 👈
+            position={place.location.coordinates} // 👈
             eventHandlers={{ click: () => showPreview(place) }}
           >
             {/* show place's title on hover the marker */}
@@ -75,6 +86,6 @@ const showPreview = (place) => {
       </MapContainer>
     </div>
   );
-}
+};
 
-export default MapContainerDiv
+export default MapContainerDiv;
--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -1,23 +1,131 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Grid, Segment } from 'semantic-ui-react';
 import MapContainerDiv from './MapContainer';
+import { MapContainer, TileLayer, Marker, Tooltip, useMap, Popup } from 'react-leaflet';
 // import { MapContainer, TileLayer } from 'react-leaflet';
 
-
 const Maps = ({ data }) => {
- 
+  data = data.filter((item) => !!item.location);
+  console.log('ddddddd', data);
+
+  //  const onClickShowMarker = (params) => {
+  //    const map = mapRef.current;
+  //    if (!map) {
+  //      return;
+  //    }
+  //    map.flyTo(MARKER_POSITION, 13);
+  //    const marker = markerRef.current;
+  //    if (marker) {
+  //      marker.openPopup();
+  //    }
+  //  }
+  const [locations, setLocations] = useState(data);
+  const [center, setCenter] = useState([51.505, -0.091]);
+  const [zoom, setZoom] = useState(11);
+  const [marker, setMarker] = useState(null);
+
+  const markerRefs = [];
+
+  const handlePopup = (marker, id) => {};
+
+  const handleClick = (id, coordinatesArr) => {
+    // setMarker(id)
+    console.log('fired', id, coordinatesArr);
+    setZoom(16);
+    setCenter(coordinatesArr);
+    console.log('refffff', markerRefs[id].openPopup());
+  };
+  const colors = [
+    'orange',
+    'yellow',
+    'olive',
+    'green',
+    'teal',
+    'blue',
+    'violet',
+    'purple',
+    'pink',
+    'brown',
+    'grey',
+    'black',
+  ];
+  // let mapRef = useRef(null);
+  // let markerRef = useRef(null);
+
+  // const onClickShowMarker = (markerLocation) => {
+  //   const map = mapRef.current;
+  //   if (!map) {
+  //     return;
+  //   }
+  //   map.flyTo(markerLocation, 13);
+  //   const marker = markerRef.current;
+  //   if (marker) {
+  //     marker.openPopup();
+  //   }
+  // };
+
   console.log('data active', data.length, 'data', data);
+  console.log('mmmmmark', markerRefs);
   return (
     <Grid columns={2} divided>
       <Grid.Row>
         <Grid.Column width={5}>
           <div>
             {data.length > 0 &&
-              data.map((dataItem) => <Segment color="red">{dataItem.title}</Segment>)}
+              data
+                .filter((item) => !!item.location)
+                .map((dataItem, index) => (
+                  <Segment
+                    color={colors[Math.floor(Math.random() * colors.length)]}
+                    style={{ cursor: 'pointer' }}
+                    className='map-card'
+                    // onClick={() => onClickShowMarker(dataItem.location.coordinates)}
+                    onClick={() =>  handleClick(index, dataItem.location.coordinates)
+                    }
+                  >
+                    {dataItem.title}
+                  </Segment>
+                ))}
           </div>
         </Grid.Column>
         <Grid.Column width={11} style={{ border: '5px red solid' }}>
-          <MapContainerDiv/>
+          {/* <MapContainerDiv
+            data={data.filter((item) => !!item.location)}
+            markerRef={markerRef}
+            mapRef={mapRef}
+          /> */}
+          <MapContainer
+            center={center}
+            zoom={zoom}
+            // scrollWheelZoom={true}
+            // whenCreated={(map) => {
+            //   mapRef.current = map;
+            // }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {locations.map((place, index) => (
+              <Marker
+                // ref={markerRef}
+                ref={(ref) => {
+                  markerRefs[index] = ref;
+                  console.log('inner', markerRefs);
+                }}
+                key={place.id}
+                position={place.location.coordinates}
+                // eventHandlers={{ click: () => showPreview(place) }}
+              >
+                {/* show place's title on hover the marker */}
+                {/* <Tooltip>{place.title}</Tooltip> */}
+                <Popup> {place.title} </Popup>
+              </Marker>
+            ))}
+            {/* <Marker position={[51.505, -0.09]}>
+          <Popup>Website Name</Popup>
+        </Marker> */}
+          </MapContainer>
         </Grid.Column>
       </Grid.Row>
     </Grid>
