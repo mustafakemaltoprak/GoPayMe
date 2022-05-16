f4f9daae913b109063770a0b6e23305e9acd1694
Daniel Sousa <dabs.sousa@gmail.com>
2022-05-16 09:58:59 +0100

    views and total raised implemented

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -11,6 +11,7 @@ import 'hammerjs';
 
 function Dashboard() {
   const { fundraisers } = useSelector((state) => state.fundraiser);
+
   const { loginSuccess } = useSelector((state) => state.user);
   const [data, setData] = useState([]);
   const [projectsCreated, setProjectsCreated] = useState(0);
@@ -27,12 +28,12 @@ function Dashboard() {
   // money per donators
   const [donatorNames, setDonatorNames] = useState([]); // ['Mustafa', 'Daniel', 'Busayo']
   const [donatorPaid, setDonatorPaid] = useState([]); // [150, 100, 200]
-  // views per project
+
+   // views per project
   const [projectViewsNames, setProjectViewsNames] = useState([]); // ['Street Artists', 'GoUkraine', 'ForFood']
   const [projectViews, setProjectViews] = useState([]); // [15, 1, 200]
 
   const [chartPickedByUser, setChartPickedByUser] = useState('');
-
 
   console.log('checking projectsCreated', projectsCreated);
   console.log('checking totalRaised', totalRaised);
@@ -47,13 +48,13 @@ function Dashboard() {
   //IMPLEMENTING PIE CHART
   ChartJS.register(ArcElement, Tooltip, Legend, Title);
   const dataPieChart = {
-    labels: ['Project 1', 'Project 2', 'Project 3', 'Project 4', 'Project 5'],
+    labels: projectNames,
     // labels: [donators],
     datasets: [
       {
         label: '# Target Amount',
         // data: [totalRaised],
-        data: [12, 55, 34, 120, 720],
+        data: projectRaised,
         backgroundColor: [
           'rgba(255, 99, 132, 0.8)',
           'rgba(54, 162, 235, 0.8)',
@@ -102,24 +103,51 @@ function Dashboard() {
     ],
   };
 
+  const dataPieChart3 = {
+    labels: projectViewsNames,
+    // labels: [donators],
+    datasets: [
+      {
+        label: '# Target Amount',
+        // data: [totalRaised],
+        data: projectViews,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.8)',
+          'rgba(54, 162, 235, 0.8)',
+          'rgba(255, 206, 86, 0.8)',
+          'rgba(75, 192, 192, 0.8)',
+          'rgba(153, 102, 255, 0.8)',
+          'rgba(255, 159, 64, 0.8)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // first load of dashboard
   if (!chartPickedByUser) {
-    setChartPickedByUser(dataPieChart)
+    setProjectNames(data.map((item) => item.title));
+    setProjectRaised(data.map((item) => item.currentAmount));
+    setChartPickedByUser(dataPieChart);
+    console.log('first loading dashboard')
   }
 
   const handleOnChange = (arg) => {
     let testObj = JSON.parse(arg.target.value);
     setChartPickedByUser(testObj);
-  }
-
+  };
 
   const labelContent = (e) => e.category;
 
-  const reactDonutChartBackgroundColor = [
-    '#00E396',
-    '#FEB019',
-    '#FF4560',
-    '#775DD0',
-  ];
+  const reactDonutChartBackgroundColor = ['#00E396', '#FEB019', '#FF4560', '#775DD0'];
 
   const reactDonutChartInnerRadius = 0.5;
   const reactDonutChartSelectedOffset = 0.04;
@@ -136,70 +164,97 @@ function Dashboard() {
 
   useEffect(() => {
     if (fundraisers.length > 0) {
-      const filteredFundraisers = fundraisers.filter(
-        (item) => item.writer === loginSuccess.userId
-      );
+      const filteredFundraisers = fundraisers.filter((item) => item.writer === loginSuccess.userId);
       setData(filteredFundraisers);
     }
     setLoaded(true);
-  }, []);
+  }, [fundraisers, loginSuccess]);
 
   useEffect(() => {
-      for (let i = 0; i < data.length; i++){
-        console.log(projectNames.length)
-      if (projectNames.length === 0) {
-        console.log("I'm in the first if")
-        setProjectNames([{name: data[i].title}])
-        console.log("after", projectNames.length)
-      } else {
-        console.log("I'm in the else")
-        setProjectNames(prevState =>[...prevState, {
-          name: data[i].title
-        },
-        ]);
+    if (loaded) {
+      setProjectNames(data.map((item) => item.title));
+      setProjectRaised(data.map((item) => item.currentAmount));
+
+      setProjectViewsNames(data.map((item) => item.title));
+      setProjectViews(data.map((item) => item.views));
+
+
+      for (let i = 0; i < data.length; i++) {
+        setTotalRaised((prev) => prev + data[i].currentAmount);
+        setDonators((prev) => prev + data[i].backers);
+        setViews((prev) => prev + data[i].views);
+        setProjectsCreated(projectsCreated + data.length);
+        setDonatorNames(data[i].prevDonations.map((item) => item.sender))
+        setDonatorPaid(data[i].prevDonations.map((item) => item.amount));
       }
-      if (projectRaised.length === 0) {
-        console.log("I'm in the first if")
-        setProjectRaised([{amount: data[i].currentAmount}])
-        console.log("after", projectRaised.length)
-      } else {
-        console.log("I'm in the else")
-        setProjectRaised(prevState =>[...prevState, {
-          amount: data[i].currentAmount
-        },
-        ]);
-      }
-      setTotalRaised((prev) => prev + data[i].currentAmount);
-      setDonators((prev) => prev + data[i].backers);
-      setViews((prev) => prev + data[i].views);
-      setProjectsCreated(projectsCreated + data.length);
     }
   }, [loaded]);
 
-  console.log("names", projectNames)
-  console.log("values", projectRaised)
+  console.log('donator names', donatorNames);
+  console.log('donator amount', donatorPaid);
 
+  // console.log(projectNames.length);
+  // if (projectNames.length === 0) {
+  //   console.log("I'm in the first if");
+  //   setProjectNames([{ name: data[i].title }]);
+  //   console.log('after', projectNames.length);
+  // } else {
+  //   console.log("I'm in the else");
+  //   setProjectNames((prevState) => [
+  //     ...prevState,
+  //     {
+  //       name: data[i].title,
+  //     },
+  //   ]);
+  // }
+
+  //**
+  //         address: "6 Caliban Mews, CV34 6FS"
+  // backers: 3
+  // categories: ['humanitarian']
+  // comments: []
+  // currentAmount: 9000
+  // deadlineDate: "2022-05-15T15:50:00.000Z"
+  // description: "give food for poor people"
+  // image: "https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
+  // likes: 1
+  // location: {type: 'Point', coordinates: Array(2), country: 'US'}
+  // prevDonations: (3) [{…}, {…}, {…}]
+  // targetAmount: 25000
+  // title: "Get food"
+  // views: 5
+  // writer: "2WWjFhvSuyclY83dsnj3oZYsqHN2"
+  // __v: 3
+  // _id: "627e53774ce2f6c29ba972c0"
+  // [[Prototype]]: Object
+
+  //
 
   const onClick = (e) => {
     console.log(e);
   };
 
-  if(!data.length) {
-    return <div>loading...</div>
+  if (!data.length) {
+    return <div>loading...</div>;
   }
 
+  console.log('names', projectNames);
+
   return (
-    <div className="profileContainer"
+    <div
+      className="profileContainer"
       style={{ border: 'red 2px solid', padding: '1rem', backgroundColor: 'white' }}
     >
-      <div className="summaryHeaderContainer"
+      <div
+        className="summaryHeaderContainer"
         style={{
           display: 'flex',
           justifyContent: 'space-between',
           width: '100%',
         }}
       >
-        <div className="summaryCard"
+        <div
+          className="summaryCard"
           style={{
             flex: '1',
             padding: '10px',
@@ -279,7 +334,8 @@ function Dashboard() {
           </div>
         </div>
       </div>
-      <div className="projectContentContainer"
+      <div
+        className="projectContentContainer"
         style={{
           border: 'black 1px solid',
           padding: '1rem',
@@ -309,29 +365,35 @@ function Dashboard() {
               )}
             </Grid.Row>
           </div> */}
-          <div className="contentRightCharts"
+          <div
+            className="contentRightCharts"
             style={{
-            position: "relative",
-            'margin-left': "0px",
-            width: "30%",
-            // border: 'orange 1px solid',
-            // margin: '5px 5px',
-            // width: '40%',
-            // height: '100vh',
+              position: 'relative',
+              'margin-left': '0px',
+              width: '30%',
+              // border: 'orange 1px solid',
+              // margin: '5px 5px',
+              // width: '40%',
+              // height: '100vh',
             }}
           >
-            <Grid columns={2} divided style={{ width: '60rem', 'justify': 'flex-end', margin: 'auto'}}>
-              <select className='chartInfo' name='chartSelected' onChange={handleOnChange} >
-                {/* <option value=""> -- Chart Selection -- </option> */}
+            <Grid
+              columns={2}
+              divided
+              style={{ width: '60rem', justify: 'flex-end', margin: 'auto' }}
+            >
+              <select className="chartInfo" name="chartSelected" onChange={handleOnChange}>
+                <option value=""> -- Chart Selection -- </option>
                 <option value={JSON.stringify(dataPieChart)}>Amount Raised by Project</option>
                 <option value={JSON.stringify(dataPieChart2)}>Amount Raised by Donators</option>
-                <option value={projectViewsNames}>Views by Project</option>
+                <option value={JSON.stringify(dataPieChart3)}>Views by Project</option>
               </select>
               <Grid.Row>
                 <Grid.Column>
-                  <Pie data={chartPickedByUser}
+                  <Pie
+                    data={chartPickedByUser}
                     options={{
-                      cutout: '90%',
+                      cutout: '70%',
                       responsive: true,
                       plugins: {
                         legend: {
@@ -344,21 +406,24 @@ function Dashboard() {
                       },
                     }}
                     style={{
-                    // height: '50%',
-                    // width: '100%',
-                    border: 'black 1px solid',
-                    // responsive: true,
-                    maintainAspectRatio: false}}
+                      // height: '50%',
+                      // width: '100%',
+                      border: 'black 1px solid',
+                      // responsive: true,
+                      maintainAspectRatio: false,
+                    }}
                   />
                 </Grid.Column>
                 <Grid.Column>
-                  <Doughnut data={dataPieChart2}
+                  <Doughnut
+                    data={dataPieChart2}
                     style={{
-                    // height: '50%',
-                    // width: '100%',
-                    border: 'black 1px solid',
-                    // responsive: true,
-                    maintainAspectRatio: false}}
+                      // height: '50%',
+                      // width: '100%',
+                      border: 'black 1px solid',
+                      // responsive: true,
+                      maintainAspectRatio: false,
+                    }}
                   />
                 </Grid.Column>
               </Grid.Row>
