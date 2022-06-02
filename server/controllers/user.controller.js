e26a2ef11f9b9e5f3d6ba1f13e7f141ac03e87fa
Obembe Busayo <damayor12@yahoo.com>
2022-06-02 09:17:58 +0200

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,14 +2,10 @@ const User = require('../models/user.model');
 
 const { generateToken } = require('../utils/generatewebtoken');
 
+
+
 const createCategories = async (req, res) => {
-  // User.findOne(req.params.id)
-  //   .then((user) => {
-  //     user.categories = req.body.categories;
-  //     user.save().then(() => res.status(201).send({ success: true }));
-  //   })
-  //   .catch((err) => res.status(400).send({ error: err.message }));
-  // console.log('key',process.env.GEOCODER_API_KEY);
+  
   try {
     const user = await User.findOneAndUpdate(
       { userId: req.params.id },
@@ -23,16 +19,14 @@ const createCategories = async (req, res) => {
   }
 };
 
+
 const loginUser = async (req, res) => {
   try {
     const { userId } = req.body;
 
     const userExists = await User.findOne({ userId }).populate('following')
-    console.log('user exists', userExists)
+    
     if (!userExists) {
-      //has not signed in before
-      //uses google sign-in
-
       const userCreated = await User.create(req.body);
       const token = generateToken(userId);
 
@@ -50,10 +44,7 @@ const loginUser = async (req, res) => {
       });
     } else {
       const token = generateToken({ userId });
-      // JSON.stringify(userExists);
-      // console.log('userexists new', userExists);
-      console.log('userexists new', userExists);
-
+  
       res.status(201).send({
         image: userExists.image,
         categories: userExists.categories,
@@ -65,19 +56,16 @@ const loginUser = async (req, res) => {
         _id: userExists._id,
         token,
       });
-      // jwt.sign({ _id }, process.env.SECRET, { expiresIn: '10d' }),
+      
     }
   } catch (error) {
     res.status(404).send({ error: error.message });
   }
 };
 
-//users/register
 const registerUser = async (req, res) => {
-  //   const { userId } = req.body;
-  try {
  
-    console.log('registering', req.body);
+  try {
     const userCreated = await User.create(req.body);
     res.status(201).send({
       image: userCreated.image,
@@ -92,16 +80,10 @@ const registerUser = async (req, res) => {
 const createNotification = async (req, res) => {
 
   try {
- 
-    console.log('registering', req.body, req.user);
-    
-
     const userFoundandUpdated = await User.findOneAndUpdate(
       { userId: req.body.targetUser },
       { $push: { notifications: req.body } }
     );
-
-    console.log('was pushed into account', userFoundandUpdated);
     res.status(201).send({
       success: true,
     });
@@ -110,16 +92,14 @@ const createNotification = async (req, res) => {
   }
 };
 
-const getUserDetails = async (req, res) => {
-  //   const { userId } = req.body;
-  try {
-    console.log('registering', req.body, req.user);
 
+const getUserDetails = async (req, res) => {
+ 
+  try {
     const userFound = await User.findOne({ userId: req.params.id })
       .populate('following')
       .populate('bookmarked');
 
-    // console.log('user', userFound);
     res.status(201).send(userFound);
   } catch (error) {
     res.status(404).send({ error: error.message });
@@ -127,10 +107,8 @@ const getUserDetails = async (req, res) => {
 };
 
 const respondToNotification = async (req, res) => {
-  //   const { userId } = req.body;
+  
   try {
-    // console.log('registering', req.body, req.user);
-
     if (req.body.response === 'accept') {
       const senderUpdated = await User.findOneAndUpdate(
         { userId: req.body.senderId },
@@ -176,10 +154,8 @@ const respondToNotification = async (req, res) => {
 };
 
 const getAccountDetails = async (req, res) => {
-  //   const { userId } = req.body;
-  try {
-    // console.log('registering', req.body, req.user);
 
+  try {
     const userFound = await User.findOne({ userId: req.user }).populate(
       'following'
     ).populate('bookmarked')
@@ -191,13 +167,11 @@ const getAccountDetails = async (req, res) => {
 };
 
 const getUserDetailsTest = async (req, res) => {
-  //   const { userId } = req.body;
-  try {
-    // console.log('registering', req.body, req.user);
 
+  try {
     let val;
     for (let i = 0; i < 10000; i++) {
-      // const element = array[i];
+      
       if (i === 7000) val = i;
     }
 
@@ -209,23 +183,19 @@ const getUserDetailsTest = async (req, res) => {
       );
       res.status(201).send({ count: userFound.test });
     }
-    // console.log(userFound);
   } catch (error) {
     res.status(404).send({ error: error.message });
   }
 };
 
-// "proxy": "http://127.0.0.1:5200",
+
 const updateAvatarPicture = async (req, res) => {
   try {
-    // console.log('Avatar Updating', req.body, req.user);
-
     const userFound = await User.findOneAndUpdate(
       { userId: req.user.userId },
       { image: req.body.image },
       { new: true }
     );
-    console.log('avatar img', req.body.image);
 
     res.status(201).send(userFound);
   } catch (error) {
